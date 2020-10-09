4b759c1776d8743c83c26e1122e380cec0208d56
LuisGA-UPM <36197267+LuisGA-UPM@users.noreply.github.com>
2020-10-09 15:39:12 +0200

    mongo

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ from base64 import b64decode
 import ast
 import logging
 import os
-import requests
-
+from pymongo import MongoClient
 from prom_lib.prometheus_client import PrometheusClient
 
 logger = logging.getLogger("AI-Agent")
@@ -16,10 +15,13 @@ logger.addHandler(stream_handler)
 
 
 def get_prometheus_data():
-    client = PrometheusClient('http://localhost:9090')
-    query = '(100 - (avg by(instance) (irate(node_cpu_seconds_total{mode="idle"}[5m])) * 100))'
+    client = PrometheusClient('http://192.168.33.20:9091')
+    query = 'osm_cpu_utilization'
     instance = '192.168.33.133:9100'
-    ts_data = client.range_query(query, instance, step=60, days=2)  # returns PrometheusData object
+    ts_data = client.range_query(query, instance, step=120, days=2) # returns PrometheusData object
+    timeseries = ts_data['result'][0]['values']
+    #with open('timeseries.json', 'w') as outfile:
+    #    json.dump(timeseries, outfile)
 
 
 if __name__ == '__main__':
@@ -27,10 +29,18 @@ if __name__ == '__main__':
     logger.info('Environment variables:\n{}'.format(os.environ))
     config = os.environ.get('config')
     # server = os.environ.get('server')
-    requests.post('http://8ca7ab4651c3.ngrok.io', data={'vnf': os.environ.get('HOSTNAME')})
+    #requests.post('http://8ca7ab4651c3.ngrok.io', data={'vnf': os.environ.get('HOSTNAME')})
+
     if config:
         config = b64decode(config)
         config = ast.literal_eval(config.decode())
         logger.info('Config:\n{}'.format(config))
     else:
         logger.info('No config available')
+
+    logger.info("Now the database:")
+    client = MongoClient('mongo', 27017)
+    osm = client['osm']
+
+    logger.info(osm.collection_names())
+
